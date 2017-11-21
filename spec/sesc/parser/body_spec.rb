837ca9f6ae031e94d0e431ad9df803262ec22282
Fernando <fernando.pereira@locaweb.com.br>
2017-11-21 18:03:48 -0200

    Add test to parser module

---
+++ spec/sesc/parser/body_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe Sesc::Parser::Body do
+  describe '#events' do
+    before(:each) do
+      VCR.use_cassette('parser/body') do
+        @html = Sesc::Crawler.new(1).get
+      end
+    end
+
+    let(:events) { described_class.new(@html).events }
+
+    it 'return an Nokogiri element' do
+      expect(events.first.class).to be Nokogiri::XML::Element
+    end
+  end
+end
---
+++ spec/sesc/parser/events_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe Sesc::Parser::Events do
+  describe '#events' do
+    before(:each) do
+      VCR.use_cassette('parser/body') do
+        @html = Sesc::Crawler.new(1).get
+      end
+    end
+
+    let(:events) { described_class.new(@html).to_hash }
+
+    it 'return an Nokogiri element' do
+      expect(events.keys).to eq ['SESC Vila Mariana']
+    end
+  end
+end
